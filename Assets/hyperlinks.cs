c5cacd36e0e108d5cd66b19b0eb2535120d4e920
Lorena Ojeda <blanca.ojeda@udgvirtual.udg.mx>
2023-11-02 20:24:08 -0600

    Initial commit

---
+++ Assets/hyperlinks.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class hyperlinks : MonoBehaviour
+{
+    public void OpenURL(string link)
+    {
+        Application.OpenURL(link);
+    }
+}
